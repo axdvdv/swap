520fed41c65d71712a0cab270e2d92e7fd6b8c76
armixi <trubnikov.r@list.ru>
2018-02-20 14:11:04 +0500

    поправил модулы и формы

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import alight from 'alight'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap';
 import './style.scss'
 
 import './router'
 // import 'controllers/app'
 import 'controllers/main'
+
+
+
 
 
 alight.bootstrap()
