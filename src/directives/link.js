b7322626c60eb7b9c87d02ce2a7a50b3e6fc370b
Pavel Ivanov <grammka@gmail.com>
2018-02-20 13:50:43 +0300

    fix route update

--- src/directives/link.js
+++ src/directives/link.js
@@ -1,10 +1,12 @@
 import alight from 'alight'
+import router from 'router'
 
 
 alight.directives.al.link = function (scope, element, expression, env) {
   $(element).on('click', function () {
     const link = $(this).attr('al-link')
 
+    router.navigate(link)
     scope.activeRoute = link
     scope.$scan()
   })
