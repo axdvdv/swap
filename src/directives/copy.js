133938cf2ec80b5807da622c49a9bd469a446556
Pavel Ivanov <grammka@gmail.com>
2018-02-21 17:55:12 +0300

    add al-copy directive

---
+++ src/directives/copy.js
@@ -0,0 +1,30 @@
+import alight from 'alight'
+
+
+alight.directives.al.copy = function (scope, element, expression, env) {
+  $(element).on('click', function () {
+    try {
+      // современный объект Selection
+      window.getSelection().removeAllRanges()
+    }
+    catch (e) {
+      // для IE8-
+      document.selection.empty()
+    }
+
+    if (document.selection) {
+      const range = document.body.createTextRange()
+      range.moveToElementText(this)
+      range.select().createTextRange()
+      document.execCommand("Copy")
+    }
+    else if (window.getSelection) {
+      const range = document.createRange()
+      range.selectNode(this)
+      console.log(range)
+      window.getSelection().addRange(range)
+      document.execCommand("Copy")
+      alert("text copied")
+    }
+  })
+}
