44ec8ab55958ab60a3dfccf9a03689c9f189e838
armixi <trubnikov.r@list.ru>
2018-02-25 14:44:40 +0500

    add history

--- src/controllers/history.js
+++ src/controllers/history.js
@@ -18,16 +18,35 @@ alight.controllers.history = function (scope) {
       const url = 'https://api.blocktrail.com/v1/tbtc/address/' + user.bitcoinData.address + '/transactions?api_key=MY_APIKEY'
 
       let total = 0
+      let transactions = []
 
       $.getJSON(url, function (r) {
         history.scope.btcTransactions = r.data
 
-        $.each(r.data, function (k, i) {
-          console.log(i)
+          $.getJSON(url, function (r) {
 
-        })
+              $.each(r.data, function (k, i) {
 
-        history.scope.$scan()
+                  transactions.push(
+                      {
+                          status:1,
+                          value: i.outputs[0].value / 100000000,
+                          address: i.outputs[0].address,
+                          date: i.time
+                      }
+                  )
+
+                  console.log(i)
+                  total += i.outputs[0].value / 100000000;
+                  //если дошли до конца обновляем все
+                  if((k+1) == r.data.length ) {
+
+                      history.scope.btcTransactions = transactions
+                      history.scope.total_btc = total
+                      history.scope.$scan()
+                  }
+              })
+          })
 
       })
     } else {
