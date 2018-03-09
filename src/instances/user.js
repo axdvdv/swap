f929e3a698d75147d58783ca3203cbe42c4dcf5b
Rustam <trubnikov.r@lsit.ru>
2018-03-09 17:26:11 +0500

    update balances

--- src/instances/user.js
+++ src/instances/user.js
@@ -26,6 +26,7 @@ class User {
     EA.once('app:ready', () => {
       this.sign()
       this.getBalances()
+      setInterval(this.getBalances, 10000) //set update balance every 10 sec
     })
   }
 
