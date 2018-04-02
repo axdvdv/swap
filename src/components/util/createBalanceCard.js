b4f946e313e5a5b4e742880c3b92db42a6297d30
Pavel Ivanov <grammka@gmail.com>
2018-04-02 23:39:36 +0700

    changes

--- src/components/util/createBalanceCard.js
+++ src/components/util/createBalanceCard.js
@@ -12,55 +12,55 @@ const createBalanceCard = (name, cb) => {
 
     return {
       template: `
-      <div class="alert alert-warning fs-16" role="alert">
-        <div class="d-flex justify-content-between">
-          <div>Баланс <strong>{{data.balance}}</strong> {{data.currency}} <a href="#" al-click="updateBalance()">refresh</a></div>
+        <div class="alert alert-warning fs-16" role="alert">
+          <div class="d-flex justify-content-between">
+            <div>Баланс <strong><truncate-amount :value="data.balance" /></strong> {{data.currency}} <a href="#" al-click="updateBalance()">refresh</a></div>
             <a href="" data-toggle="modal" data-target="#{{data.modal_id}}">withdraw money</a>
+          </div>
+          <hr />
+          <a href="" al-copy title="copy to clipboard">{{data.address}}</a>
         </div>
-        <hr />
-        <a href="" al-copy title="copy to clipboard">{{data.address}}</a>
-      </div>
-      
-      <div class="modal fade" id="{{data.modal_id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <form action="" al-submit="withdraw()">
-            <div class="modal-content">
+        
+        <div class="modal fade" id="{{data.modal_id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <form action="" al-submit="withdraw()">
+              <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" >withdraw {{data.currency}}</h4>
-                    <button type="button" class="close"  data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title" >withdraw {{data.currency}}</h4>
+                  <button type="button" class="close"  data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
-                    <div class="text-danger">
+                  <div class="text-danger">
+                  </div>
+                  <div class="form-group">
+                    <label>Address</label>
+                    <div class="input-group mb-3">
+                      <input class="form-control" value=""  pattern="{{pattern}}" al-value="data.withdraw_address" required=""   type="text" placeholder="Address" onkeyup="this.value = this.value.trim()">
                     </div>
-                    <div class="form-group">
-                        <label>Address</label>
-                        <div class="input-group mb-3">
-                            <input class="form-control" value=""  pattern="{{pattern}}" al-value="data.withdraw_address" required=""   type="text" placeholder="Address" onkeyup="this.value = this.value.trim()">
-                        </div>
+                  </div>
+        
+                  <div class="form-group">
+                    <label>Amount</label>
+                    <div class="input-group mb-3">
+                      <input class="form-control" onkeyup="this.value=this.value.match(/^[\\d.]+$/)"  value="0" al-change="(data.amount > data.balance)?(data.amount=data.balance):''"  al-init="data.amount = data.min_amount"  al-value="data.amount" required=""   type="text"  >
+                      <div class="input-group-append">
+                        <span class="input-group-text">{{data.currency}}</span>
+                      </div>
+        
                     </div>
-
-                    <div class="form-group">
-                        <label>Amount</label>
-                        <div class="input-group mb-3">
-                            <input class="form-control" onkeyup="this.value=this.value.match(/^[\\d.]+$/)"  value="0" al-change="(data.amount > data.balance)?(data.amount=data.balance):''"  al-init="data.amount = data.min_amount"  al-value="data.amount" required=""   type="text"  >
-                            <div class="input-group-append">
-                                <span class="input-group-text">{{data.currency}}</span>
-                            </div>
-
-                        </div>
-                        <p class="list-text">min: <a href="" @click="data.amount=data.min_amount">{{data.min_amount}}</a>, max <a href="" @click="data.amount=data.balance">{{data.balance}}</a>
-                        </p>
-                    </div>
+                    <p class="list-text">min: <a href="" @click="data.amount=data.min_amount">{{data.min_amount}}</a>, max <a href="" @click="data.amount=data.balance">{{data.balance}}</a>
+                    </p>
+                  </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary"  data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary" al-attr.disabled="disabled">Transfer</button>
+                  <button type="button" class="btn btn-secondary"  data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary" al-attr.disabled="disabled">Transfer</button>
                 </div>
-            </div>
-        </form>
-    </div>
-</div>
-    `
+              </div>
+            </form>
+          </div>
+        </div>
+      `
     }
   })
 }
