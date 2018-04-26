2e4c879dd260922e91582da195c3600cf67b860e
Noxon <2914674+noxonsu@users.noreply.github.com>
2018-04-26 17:04:03 +0300

    more decentralisation more

--- src/instances/ipfs.js
+++ src/instances/ipfs.js
@@ -10,7 +10,8 @@ const ipfs = new Ipfs({
       Swarm: [
         // '/ip4/46.101.244.101/tcp/9090/ws/p2p-websocket-star/',
         // '/ip4/146.185.173.84/tcp/9090/ws/p2p-websocket-star/',
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+        '/dns4/star.wpmix.net/tcp/443/wss/p2p-websocket-star'
       ],
     },
   },
